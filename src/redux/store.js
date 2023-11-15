562a90980070f012d8c1ae97d96faebc2e7ba454
sharekim <sksyag@naver.com>
2023-11-15 16:38:37 +0900

    product filter 생성

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authRecuder from "./slice/authSlice";
 import productReducer from "./slice/productSlice";
+import filterReducer from "./slice/filterSlice";
 
 const rootReducer = combineReducers({
   auth: authRecuder,
   product: productReducer,
+  filter: filterReducer,
 });
 
 const store = configureStore({
