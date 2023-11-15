78448bb22b886bb529be336e9a11d4cf9c4d7b4d
sharekim <sksyag@naver.com>
2023-11-15 12:24:01 +0900

    firebase로부터 product 데이터 가져오기

--- src/hooks/useFetchCollection.jsx
+++ src/hooks/useFetchCollection.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
 
-const useFetchCollection = () => {
-  return <div>useFetchCollection</div>;
+import React, { useState, useEffect, useCallback } from "react";
+
+import { db } from "@/firebase/firebase";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const useFetchCollection = (collectionName) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getCollection = useCallback(() => {
+    setIsLoading(true);
+    try {
+      const docRef = collection(db, collectionName);
+      const q = query(docRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        const allData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setData(allData);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  }, [collectionName]);
+
+  useEffect(() => {
+    getCollection();
+  }, [getCollection]);
+
+  return { data, isLoading };
 };
 
 export default useFetchCollection;
