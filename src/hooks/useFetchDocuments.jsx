3303f3f4e53586403328576820dd1d17542c2694
sharekim <sksyag@naver.com>
2023-11-16 15:53:01 +0900

    document 활용을 위한 util 작성

--- src/hooks/useFetchDocuments.jsx
+++ src/hooks/useFetchDocuments.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+import { db } from "@/firebase/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import React, { useCallback, useEffect } from "react";
 
-const useFetchDocuments = () => {
-  return <div>useFetchDocuments</div>;
+const useFetchDocuments = (collectionName, arg) => {
+  const [document, setDocument] = useState([]);
+  const getDocuments = useCallback(async () => {
+    const q = query(
+      collection(db, collectionName),
+      where(arg[0], arg[1], arg[2])
+    );
+    const querySnapshot = await getDocs(q);
+    let documentsArray = [];
+
+    querySnapshot.forEach((doc) => {
+      documentsArray.push(doc.data());
+    });
+
+    setDocument(documentsArray);
+  }, [collectionName, arg[0], arg[1], arg[2]]);
+
+  useEffect(() => {
+    getDocuments();
+  }, [getDocuments]);
+
+  return document;
 };
 
 export default useFetchDocuments;
