e08caac271241d56d229be7027af4849f995bfd7
sharekim <sksyag@naver.com>
2023-11-16 13:29:43 +0900

    firebase 도메인 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["plus.unsplash.com", "images.unsplash.com"],
+    domains: [
+      "plus.unsplash.com",
+      "images.unsplash.com",
+      "firebasestorage.googleapis.com",
+    ],
   },
 };
 
