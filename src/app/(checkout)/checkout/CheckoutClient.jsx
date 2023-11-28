fd816e82ea11912daa0623a1a65323cea1abca90
sharekim <sksyag@naver.com>
2023-11-28 17:22:51 +0900

    토스 결제 구현

--- src/app/(checkout)/checkout/CheckoutClient.jsx
+++ src/app/(checkout)/checkout/CheckoutClient.jsx
@@ -1,13 +1,91 @@
 "use client";
-
+import React from "react";
 import Button from "@/components/button/Button";
 import CheckoutForm from "@/components/checkoutForm/CheckoutForm";
 import Heading from "@/components/heading/Heading";
-import React from "react";
+import { db } from "@/firebase/firebase";
+import { selectEmail, selectUserID } from "@/redux/slice/authSlice";
+import {
+  CLEAR_CART,
+  selectCartItems,
+  selectCartTotalAmount,
+} from "@/redux/slice/cartSlice";
+import { selectShippingAddress } from "@/redux/slice/checkoutSlice";
+import { loadTossPayments } from "@tosspayments/payment-sdk";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import styles from "./Checkout.module.scss";
 
 const CheckoutClient = () => {
-  const handleSubmit = () => {};
+  const userID = useSelector(selectUserID);
+  const cartItems = useSelector(selectCartItems);
+  const shippingAddress = useSelector(selectShippingAddress);
+  const userEmail = useSelector(selectEmail);
+  const cartTotalAmount = useSelector(selectCartTotalAmount);
+
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const tossPayment = await loadTossPayments(
+      process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY
+    );
+
+    tossPayment
+      .requestPayment("카드", {
+        amount: cartTotalAmount,
+        orderId: Math.random().toString(36).slice(2),
+        orderName: "주문",
+      })
+      .then(async function (data) {
+        const { orderId, paymentKey, amount } = data;
+        const secretKey = process.env.NEXT_PUBLIC_TOSS_SECRET_KEY;
+        const url = `https://api.tosspayments.com/v1/payments/confirm`;
+        const basicToken = Buffer.from(`${secretKey}:`, "utf-8").toString(
+          "base64"
+        );
+        const confirmResponse = fetch(url, {
+          method: "post",
+          body: JSON.stringify({ amount, orderId, paymentKey }),
+          headers: {
+            Authorization: `Basic ${basicToken}`,
+            "Content-Type": "application/json",
+          },
+        }).then((response) => response.json());
+
+        console.log("confirmResponse", confirmResponse);
+
+        const today = new Date();
+        const date = today.toDateString();
+        const time = today.toLocaleDateString();
+
+        const orderData = {
+          userID,
+          userEmail,
+          orderDate: date,
+          orderTime: time,
+          orderAmount: amount,
+          orderStatus: "주문수락",
+          cartItems,
+          shippingAddress,
+          createdAt: Timestamp.now().toDate(),
+        };
+
+        await addDoc(collection(db, "orders", orderData));
+        dispatch(CLEAR_CART());
+
+        router.push(`/checkout-success?orderId=${orderId}`);
+      })
+      .catch((error) => {
+        if (error.code === "USER_CANCEL") {
+          toast.error("결제창이 닫혔습니다.");
+        }
+      });
+  };
 
   return (
     <section>
